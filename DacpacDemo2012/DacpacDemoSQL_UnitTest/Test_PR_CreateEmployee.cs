bb00d076e432ab379638f64e6bd9ac9022fa7b32
Snktheone <sarang@accedius.com>
2016-11-16 20:47:40 +0000

    Added - Unit Testing Project

---
+++ DacpacDemo2012/DacpacDemoSQL_UnitTest/Test_PR_CreateEmployee.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DacpacDemoSQL_UnitTest
+{
+    [TestClass()]
+    public class Test_PR_CreateEmployee : SqlDatabaseTestClass
+    {
+
+        public Test_PR_CreateEmployee()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction dbo_PR_CreateEmployeeTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test_PR_CreateEmployee));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.InconclusiveCondition inconclusiveCondition1;
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction testInitializeAction;
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.ChecksumCondition TestDataExists;
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.RowCountCondition rowCountCondition1;
+            this.dbo_PR_CreateEmployeeTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            dbo_PR_CreateEmployeeTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            inconclusiveCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.InconclusiveCondition();
+            testInitializeAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            TestDataExists = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.ChecksumCondition();
+            rowCountCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.RowCountCondition();
+            // 
+            // dbo_PR_CreateEmployeeTestData
+            // 
+            this.dbo_PR_CreateEmployeeTestData.PosttestAction = null;
+            this.dbo_PR_CreateEmployeeTestData.PretestAction = null;
+            this.dbo_PR_CreateEmployeeTestData.TestAction = dbo_PR_CreateEmployeeTest_TestAction;
+            // 
+            // dbo_PR_CreateEmployeeTest_TestAction
+            // 
+            dbo_PR_CreateEmployeeTest_TestAction.Conditions.Add(inconclusiveCondition1);
+            resources.ApplyResources(dbo_PR_CreateEmployeeTest_TestAction, "dbo_PR_CreateEmployeeTest_TestAction");
+            // 
+            // inconclusiveCondition1
+            // 
+            inconclusiveCondition1.Enabled = true;
+            inconclusiveCondition1.Name = "inconclusiveCondition1";
+            // 
+            // testInitializeAction
+            // 
+            testInitializeAction.Conditions.Add(TestDataExists);
+            testInitializeAction.Conditions.Add(rowCountCondition1);
+            resources.ApplyResources(testInitializeAction, "testInitializeAction");
+            // 
+            // TestDataExists
+            // 
+            TestDataExists.Checksum = "-1929658088";
+            TestDataExists.Enabled = true;
+            TestDataExists.Name = "TestDataExists";
+            // 
+            // rowCountCondition1
+            // 
+            rowCountCondition1.Enabled = true;
+            rowCountCondition1.Name = "rowCountCondition1";
+            rowCountCondition1.ResultSet = 1;
+            rowCountCondition1.RowCount = 1;
+            // 
+            // Test_PR_CreateEmployee
+            // 
+            this.TestInitializeAction = testInitializeAction;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod()]
+        public void dbo_PR_CreateEmployeeTest()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_PR_CreateEmployeeTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions dbo_PR_CreateEmployeeTestData;
+    }
+}
